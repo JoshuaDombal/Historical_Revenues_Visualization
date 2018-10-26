10f0e6d87f873205a7b1b4f27443da2e42482a51
JoshDombal <joshua.dombal@outlook.com>
2018-10-25 17:18:16 -0700

    Added two visualizations

--- DataVis.py
+++ DataVis.py
@@ -4,9 +4,51 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import csv
+import seaborn as sns
 
 
-data = pd.read_excel('Historical_Revenues.xlsx')
+file = 'Historical_Revenues.xlsx'
+
+xl = pd.ExcelFile(file)
+
+print(xl.sheet_names)
 
 
-print(data.describe())
+dataframe = xl.parse('Pivot Table Raw Data')
+"""
+print(dataframe.info())
+
+dataframe.plot(kind='scatter', x=')
+plt.show()
+
+"""
+
+# Shows Fiscal Year with highest Revenue
+a = sns.barplot(x="Fiscal Year", y="Amount", data=dataframe)
+for item in a.get_xticklabels():
+    item.set_rotation(80)
+plt.show()
+
+
+
+################################################################################
+
+
+#sns.boxplot(x="Fiscal Year", y="Amount", data=dataframe)
+#plt.show()
+
+
+# Shows Sum of Categories
+# Helps show which category had the most revenue overall
+b = sns.barplot(x="Category", y="Amount", data=dataframe)
+plt.show()
+
+
+
+
+#print(data.head())
+
+#colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'red']
+
+#data['Type of Fund'].value_counts().plot(kind='pie', title='TOF', colors=colors)
+#plt.show()
